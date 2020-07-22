436e880a90de2a6b9c02d4adb4d765bb21b08f37
ryusuke06 <or.06.012@gmail.com>
2020-07-22 21:20:20 +0900

    整理しただけ

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),//このアロー関数の意味は引数Appの内容をレンダリングする
-}).$mount('#app')//$mountメソッドはelプロパティと同じく指定したタグにVueオブジェクトをマウントする役割

1baa858dee4c0e8c7c6cc131466cdf070c98eaea
ryusuke06 <or.06.012@gmail.com>
2020-08-12 11:40:55 +0900

    トップページサンプル作成

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,33 +1,32 @@
 <template>
   <div class="font">
-    <p>{{ result }}Hello</p>
-  	<nuxt-link to="/users/login">ログイン</nuxt-link>
-    <ul id="dqmenu">
-      <li class="top">ドラクエメニューの上枠</li>
-      <li class="menu01"><a href="リンク先のURL">たたかう</a></li>
-      <li class="menu06"><a href="リンク先のURL">はなす</a></li>
-      <li class="menu14"><a href="リンク先のURL">ごあいさつ</a></li>
-      <li class="menu16"><a href="リンク先のURL">おといあわせ</a></li>
-      <li class="menu19"><a href="リンク先のURL">つかいかた</a></li>
-      <li class="menu20"><a href="リンク先のURL">おしらせ</a></li>
-      <li class="menu23"><a href="リンク先のURL">プロフィール</a></li>
-      <li class="menu29"><a href="リンク先のURL">イベント</a></li>
-      <li class="menu31"><a href="リンク先のURL">ログイン</a></li>
-    </ul>
     <div class="blackboard question">
-      <p>＊「ここに質問が入ります」</p>
+      <p>＊「このおいぼれに何のようじゃ？」</p>
     </div>
-    <div class="clearfix">
-      <div class="blackboard answer float-r">
-        <p class="blink-before">はい</p>
-        <p>いいえ</p>
+    <v-img
+    :src="(require('../assets/imgs/castle.jpg'))"
+    height="450">
+      <v-row
+        :align="center"
+        :justify="center"
+      >
+        <v-img
+        :src="(require('../assets/imgs/last.gif'))"
+        height="350"
+        contain
+        ></v-img>
+      </v-row>
+      <div class="clearfix">
+        <div class="blackboard answer float-r">
+          <span><p class="blink-before">はい</p></span>
+          <span><p class="blink-before">いいえ</p></span>
+        </div>
       </div>
-    </div>
+    </v-img><!-- height固定、containなし（画像全体を含めないこと）v-imgでの画像の参照は上記のようにする -->
   </div>
 </template>
 
 <script>
-import Dqmenu from '@/assets/css/dqmenu.css'
 import BlackFrame from '@/assets/css/black_frame.css'
 
 export default {
